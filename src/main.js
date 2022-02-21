7ca265148325c488e2003bf0f028095e03008885
StefanieAviles <stefanie42019@gmail.com>
2022-02-21 16:12:45 -0500

    Existe un problema en la linea 25 main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import App from './components/App.js';
 
-<<<<<<< HEAD
-//document.getElementById('root').appendChild(App());
-
 const first_window = document.getElementById('first_window');
 const second_window = document.getElementById('second_window');
 const fourth_window = document.getElementById('fourth_window');
@@ -11,6 +8,7 @@ const header_windows = document.getElementById('header');
 const startButton = document.getElementById('Btn1');
 const continueButton = document.getElementById('Btn2');
 
+const numCard=4;
 startButton.addEventListener('click', ()=> {
     console.log('entre');
     second_window.classList.add("show");
@@ -20,21 +18,20 @@ startButton.addEventListener('click', ()=> {
 for (let index = 0; index <numCard; index++) {
     document.getElementById('root').appendChild(App(numCard)[index]);
 }
+
+App(numCard).forEach(element => {
+    document.getElementById('root').appendChild(element);
+}); 
+document.getElementsByClassName('Card').forEach(function(element){
+    element.addEventListener('click', myFunction);
+});
+
+function myFunction(){
+    console.log("1");
+}
 continueButton.addEventListener('click', ()=> {
     second_window.classList.remove("show");
     header_windows.classList.add("showheader");
     fourth_window.classList.add("show");
 });
-const numCard=4;
-
-App(numCard).forEach(element => {
-    document.getElementById('root').appendChild(element);
-}); 
-document.getElementsByClassName('Card').forEach(function(element) {
-    element.addEventListener('click', myFunction);
-  });
-
-  function myFunction(){
-      console.log("1");
-  }
 
