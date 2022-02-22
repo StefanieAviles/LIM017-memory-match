8a41830dac6b883b43ce749049efb02e10a8aa79
StefanieAviles <stefanie42019@gmail.com>
2022-02-22 10:32:14 -0500

    Inicio de construccion de la tercera ve3era ventana donde eligen el tema y nivel

--- src/main.js
+++ src/main.js
@@ -22,9 +22,9 @@ for (let index = 0; index <numCard; index++) {
 App(numCard).forEach(element => {
     document.getElementById('root').appendChild(element);
 }); 
-document.getElementsByClassName('Card').forEach(function(element){
+/*document.getElementsByClassName('Card').forEach(function(element){
     element.addEventListener('click', myFunction);
-});
+});*/
 
 function myFunction(){
     console.log("1");
