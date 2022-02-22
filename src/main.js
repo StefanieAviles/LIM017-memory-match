4ac819b2f7db75529068e29dbeb6c20cde0006e9
Tenshi <64874044+Myriam555@users.noreply.github.com>
2022-02-22 10:03:52 -0500

    Crear evento Click de elemento Card

--- src/main.js
+++ src/main.js
@@ -4,12 +4,10 @@ const numCard=4;
 
 App(numCard).forEach(element => {
     document.getElementById('root').appendChild(element);
-}); 
-document.getElementsByClassName('Card').forEach(function(element) {
     element.addEventListener('click', myFunction);
-  });
+}); 
 
   function myFunction(){
-      console.log("1");
+      console.log("");
   }
 
